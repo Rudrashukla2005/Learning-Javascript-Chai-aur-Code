ebca2160343699b333bde5d8b6b5b1021d2f3dfa
Rudrashukla2005 <shuklarudra2005@gmail.com>
2025-06-02 12:27:17 +0000

    Learning Arrays

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,7 @@
+//Arrays
+
+const arrays_numbers = [0,1,2,4,4,5,6,7,7,8,8];
+const Heroes = ["Hritik", "Nana Patekar", "Buman Irani", "Malhar Thacker","Yash Soni"];
+
+const arrays = new Array(0,1,2,3,4,5);
+console.log(arrays[1]);
