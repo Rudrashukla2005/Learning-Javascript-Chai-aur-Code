74449f4a4c12611e13b4c5ea62c5dfe39f7c5a2e
Rudrashukla2005 <shuklarudra2005@gmail.com>
2025-06-02 12:54:30 +0000

    Baiscs about arrays-01

--- 02_basics/01_Arrays.js
+++ 02_basics/01_Arrays.js
@@ -5,3 +5,15 @@ const Heroes = ["Hritik", "Nana Patekar", "Buman Irani", "Malhar Thacker","Yash 
 
 const arrays = new Array(0,1,2,3,4,5);
 console.log(arrays[1]);
+console.log(Heroes[0]);
+
+console.log(arrays_numbers.push(6));
+console.log(arrays_numbers);
+console.log(arrays_numbers.pop());
+console.log(arrays_numbers);
+console.log(arrays_numbers.unshift(9));
+console.log(arrays_numbers);
+console.log(arrays_numbers.shift());
+console.log(arrays_numbers);
+
+// indexOf and includes, slice and splice operations can be easily executed with the arrays too.
